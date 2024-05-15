d94c2cdde1b18570a73422a08ddb33627d08fb9c
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-05-15 20:40:44 +0500

    correctotlekcii

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         int begin = 100;
         int amount = 1100;
-        int bonus = 0;
-        int result = 0;
+        int bonus;
 
         if (amount >= 1000) {
             bonus = amount / 100;
-        }
-        result = begin + amount + bonus;
+        } else
+            bonus = 0;
+
+        int result = begin + amount + bonus;
         if (bonus > 0) {
             System.out.println("сумма пополнения " + amount + ", бонус " + bonus + ", итоговая сумма " + result);
         } else {
